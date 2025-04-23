d26bcf7e07caf87dd09f60dd6c4e70dc818a5c41
Avah Whitehead <avah.r.whitehead@gmail.com>
2025-04-23 22:41:56 +0100

    Added very basic layout for scores page

--- src/pages/scorespage.py
+++ src/pages/scorespage.py
@@ -1,8 +1,9 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import StringVar
 
 import darkdetect
 import sv_ttk
+import tksheet
 
 
 class ScoresPage(tk.Frame):
@@ -10,8 +11,51 @@ class ScoresPage(tk.Frame):
 		tk.Frame.__init__(self, parent, *args, **kwargs)
 		self.parent = parent
 
-		button = ttk.Button(parent, text="Click me on the user page!")
-		button.pack()
+		# Root layout
+		self.grid_columnconfigure(0, weight=1)
+		self.grid_rowconfigure(0, weight=0, minsize=40)
+		self.grid_rowconfigure(1, weight=1, minsize=40)
+
+		# Input fields
+		input_frame = tk.Frame(self, bg="#00FF00")
+		input_frame.grid_rowconfigure(0, weight=1)
+		input_frame.grid_rowconfigure(1, weight=1)
+		input_frame.grid(row=0, column=0, sticky="nsew")
+
+		name_label_var = StringVar()
+		name_label_var.set("Name")
+
+		name_label = tk.Label(input_frame, textvariable=name_label_var)
+		name_label.grid(row=0, column=0, sticky="w")
+
+		name_input = tk.Entry(input_frame)
+		name_input.grid(row=1, column=0, sticky="nsew")
+
+		score_label_var = StringVar()
+		score_label_var.set("Score")
+
+		score_label = tk.Label(input_frame, textvariable=score_label_var)
+		score_label.grid(row=0, column=1, sticky="w")
+
+		score_input = tk.Entry(input_frame)
+		score_input.grid(row=1, column=1, sticky="nsew")
+
+		# Display grid
+		sheet_frame = tk.Frame(self, bg="#FF0000")
+		sheet_frame.grid(row=1, column=0, sticky="nsew")
+
+		sheet_frame.grid_columnconfigure(0, weight=1)
+		sheet_frame.grid_rowconfigure(0, weight=1)
+
+		self.sheet = tksheet.Sheet(sheet_frame,
+								   data=[[f"Row {r}, Column {c}\nnewline1\nnewline2" for c in range(5)] for r in range(500)],
+								   auto_resize_rows=10,
+								   auto_resize_columns=100,
+								   vertical_grid_to_end_of_window=True
+								   )
+		self.sheet.enable_bindings()
+
+		self.sheet.grid(row=0, column=0, sticky="nsew", padx=10, pady=10)
 
 
 if __name__ == "__main__":
