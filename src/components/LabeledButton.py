ca6dfee3bae115565e2fcd209cea7ac67208fd40
Avah Whitehead <avah.r.whitehead@gmail.com>
2025-05-04 00:01:55 +0100

    Added border colour around button on focus

--- src/components/LabeledButton.py
+++ src/components/LabeledButton.py
@@ -21,3 +21,15 @@ class LabeledButton(tk.Frame):
 		self.button_var.set(text)
 		self.button = ctk.CTkButton(self, textvariable=self.button_var, command=command, *args, **kwargs)
 		self.button.grid(row=1, column=0, sticky="nsew", padx=0, pady=0)
+
+		self.button.configure(border_width=0)
+
+		self.bind("<FocusIn>", self._on_focus)
+		self.bind("<FocusOut>", self._on_focus_out)
+
+	def _on_focus(self, e=None):
+		self.button.configure(border_color=self.button.cget('hover_color'))
+		self.button.configure(border_width=3)
+
+	def _on_focus_out(self, e=None):
+		self.button.configure(border_width=0)
